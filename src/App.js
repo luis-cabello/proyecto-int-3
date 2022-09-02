de47d124c723c80860ac1befabdedc80dd9d25d8
luis-cabello <lcabello@udesa.edu.ar>
2022-09-02 17:05:13 -0300

    l

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Switch, Link } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
