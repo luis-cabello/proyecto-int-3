ea64792997418a6d9a509721ddacc35ab2e39e3b
Mati2108 <matiascalo2@gmail.com>
2022-09-12 16:38:10 -0300

    todo vermas y mas

--- src/components/VerTodas/VerTodas.js
+++ src/components/VerTodas/VerTodas.js
@@ -8,9 +8,11 @@ class VerTodas extends Component {
         super(props);
         this.state = {
             dataPelicula: [],
-            nextUrl : '',
+            nextUrl : 1,
             dataSeries: [],
-            backup: []
+            backup: [],
+            backupSeries: [], 
+            datos: []
         }
     }
 
@@ -22,8 +24,9 @@ class VerTodas extends Component {
             .then(data => this.setState(
 
                 { dataPelicula: data.results,
-                   nextUrl: data.next,
-                   backup: data.results }
+                   nextUrl: data.page + 1,
+                   backup: data.results,
+                datos: data }
                 // nextUrl: dataPelicula.info.next} 
             ))
             .catch(error => console.log('el error fue ' + error))
@@ -36,7 +39,8 @@ class VerTodas extends Component {
         fetch("https://api.themoviedb.org/3/tv/popular?api_key=7a176cc95147be6e695be2faf0e8ff9c ")
             .then(response => response.json())
             .then(data => this.setState(
-                { dataSeries: data.results }
+                { dataSeries: data.results, 
+                backupSeries: data.results}
             ))
             .catch(error => console.log('el error fue ' + error))
 
@@ -44,49 +48,63 @@ class VerTodas extends Component {
     }
     traerMas(){
         //Traer la siguiente página de personajes
-        fetch(this.state.nextUrl)
-            .then( res => res.json())
-            .then( data => this.setState({
-                dataPelicula: data.results.concat(this.state.dataPelicula),
-                nextUrl: data.next
-            }))
-            .catch()
+        
+        fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=7a176cc95147be6e695be2faf0e8ff9c&language=en-US&page=${this.state.nextUrl}`)
+        .then( res => res.json())
+        .then( data => this.setState({
+            dataPelicula: data.results.concat(this.state.dataPelicula),
+            nextUrl: data.page
+        }))
+        .catch()
     }
-
-
+    
+    
     filtrarPelicula(nombre) {
         let arrayFiltrado =
-            this.state.backup.filter
-                (pelicula => pelicula.title.toLowerCase().includes(nombre.toLowerCase()))
-
+        this.state.backup.filter
+        (pelicula => pelicula.title.toLowerCase().includes(nombre.toLowerCase()))
+        
         this.setState({
             dataPelicula: arrayFiltrado
-
+            
         })
-
+        
     }
-
-
-
-
-    //}
-    render() {
-        return (
-
-            <React.Fragment>
+    
+    filtrarSeries(nombre) {
+        let arrayFiltrado =
+        this.state.backupSeries.filter
+                (serie => serie.name.toLowerCase().includes(nombre.toLowerCase()))
+                
+                this.setState({
+                    dataSeries: arrayFiltrado
+                    
+                })
+                
+            }
+            
+            
+            //}
+            render() {
+                console.log(this.state.nextUrl);
+                console.log(this.state.datos);
+                return (
+                    
+                    <React.Fragment>
                 <div>
                     <Filtro filtro={(nombre) => this.filtrarPelicula(nombre)} />
                     <h2 className="TituloC">Movies</h2>
+                    <button className = 'button-54' onClick={()=>this.traerMas(this.state.nextUrl + 1)}> Traer más </button>
                 </div>
                 <section className='card-container'>
                     {this.state.dataPelicula.map((unPelicula, idx) => <PeliculaCard key={unPelicula + idx} data={unPelicula} image={unPelicula.poster_path} title={unPelicula.title} descripcion={unPelicula.overview} />)}
-                    <button className = 'button-54' onClick={()=>this.traerMas()}> Traer más </button>
                 </section>
 
 
 
 
                 <div>
+                <Filtro filtro={(nombre) => this.filtrarSeries(nombre)} />
                     <h2 className="TituloC">Series</h2>
                 </div>
                 <section className='card-container'>
